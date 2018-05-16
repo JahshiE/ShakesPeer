4180a2c26973add5f219e12b4dfd7ead5ebeacbc
Jahsh <joshua.espinas@gmail.com>
2018-05-16 22:07:21 +1200

    Added documentation to javascript function and moved <script> tg to bottom of <body>.

--- js/shakespeer.js
+++ js/shakespeer.js
@@ -1,24 +1,30 @@
+// Default tab is my_solution
 document.getElementById("default_open").click();
 
-
 function showSolution(event, name_solution) {
-
+    /* Shows the solution content for a specfic name selected in the solution navigator. 
+        inputs: event -> event object
+                name_solution -> id of article tag to select.
+        outputs: no return.
+    */
 
     var i, solution_content, solution_tabs;
 
-
+    // Setting all articles to display nothing
     solution_content = document.getElementsByTagName("article");
     for (i = 0; i < solution_content.length; i++) {
         solution_content[i].style.display = "none";
     }
 
+    // Setting all solution tabs to be inactive.
     solution_tabs = document.getElementsByClassName("solution_tab");
     for (i = 0; i < solution_tabs.length; i++) {
         solution_tabs[i].className = solution_tabs[i].className.replace(" active", "")
     }
 
+    // Displaying the selected solution content
     document.getElementById(name_solution).style.display = "block";
-    // alert(event.currentTarget.className);
     event.currentTarget.className += " active";
 }
+
 
